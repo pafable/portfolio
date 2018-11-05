eb64bc3646ac91e9bde0aa7c0f4a0cf55c03f21e
pafable <39135798+pafable@users.noreply.github.com>
2018-11-05 13:26:49 -0500

    comments added

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express");
 var app = express();
 
+// Ignores .ejs extension
 app.set("view engine", "ejs");
 
+// when "/" is requested, client will see "hello World"
 app.get("/", function(req,res){
     res.send("Hello World");
 });
 
+// forces node to start app and listen on default port and ip 
 app.listen(process.env.PORT, process.env.IP, function(req,res){
     console.log("####################");
     console.log("## SERVER STARTED ##");
