40d8a72c0649b2795f530c0756e5786bb7141262
pafable <39135798+pafable@users.noreply.github.com>
2018-11-05 13:24:03 -0500

    base app.js

--- app.js
+++ app.js
@@ -6,3 +6,9 @@ app.set("view engine", "ejs");
 app.get("/", function(req,res){
     res.send("Hello World");
 });
+
+app.listen(process.env.PORT, process.env.IP, function(req,res){
+    console.log("####################");
+    console.log("## SERVER STARTED ##");
+    console.log("####################");
+});
