90a7a356d033d16b77ca02a8932189e8e9598e3a
pafable <39135798+pafable@users.noreply.github.com>
2018-11-13 22:49:42 -0500

    add 404 page

--- app.js
+++ app.js
@@ -15,10 +15,6 @@ app.get("/hello", function(req,res){
     res.send("hello world");
 });
 
-app.use(function(req, res, next) {
-    res.send("404");
-});
-
 // Separated routes into routes dir
 app.use(indexRoutes);
 app.use(projectsRoutes);
